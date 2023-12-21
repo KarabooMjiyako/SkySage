19abc2b59bae85cfd0cdfe28c7840c4f617bec7b
Karabo Mjiyako <149914891+KarabooMjiyako@users.noreply.github.com>
2023-12-21 20:13:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-function weatherData(city) {
-    const apiKey = '29ca9b932a2508dt10ce6d3564ofaf97';
-    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  
-    axios.get(apiUrl)
-      .then(response => {
-        const data = response.data;
-        updateInterface(data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }
-  
-  function updateInterface(data) {
-    const weatherMain = data?.weather?.[0]?.main || null;
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.current.temp_c;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#windSpeed");
+    let timeElement = document.querySelector("#current-date");
+    let imageElement = document.querySelector('#icon');
 
-    const image = document.querySelector('.weather-box img');
     const weatherImages = {
         'Clear': 'https://i.ibb.co/0XPnVdc/clear.png',
         'Rain': 'https://i.ibb.co/FxmRy5n/rain.png',
@@ -23,59 +15,65 @@ function weatherData(city) {
         'Clouds': 'https://i.ibb.co/R7rNsGd/cloud.png',
         'Mist': 'https://i.ibb.co/gvGqwG5/mist.png'
     };
-    image.src = weatherImages[weatherMain] || 'https://i.ibb.co/R7rNsGd/cloud.png';
 
-    const cityElement = document.querySelector('#city');
-    const temperatureElement = document.querySelector('#temperature');
-    const descriptionElement = document.querySelector('#description');
-    const humidityElement = document.querySelector('#humidity');
-    const windElement = document.querySelector('#windSpeed');
+    let weatherMain = response.data.current.condition.text;
+    imageElement.innerHTML = `<img src="${weatherImages[weatherMain] || 'https://i.ibb.co/R7rNsGd/cloud.png'}" class="weather-app-icon" alt="${weatherMain || 'Unknown Weather'}" />`;
 
-    cityElement.innerHTML = data?.location?.name || 'Unknown City';
-    temperatureElement.innerHTML = `${data?.current?.temp_c} °C` || 'Unknown Temperature';
-    descriptionElement.innerHTML = data?.current?.condition?.text || 'Unknown Description';
-    humidityElement.innerHTML = `${data?.current?.humidity}%` || 'Unknown Humidity';
-    windElement.innerHTML = `${data?.wind?.speed} Km/h` || 'Unknown Wind Speed';
+    cityElement.innerHTML = response.data.location.name;
+    timeElement.innerHTML = formatDate(new Date(response.data.location.localtime_epoch * 1000));
+    descriptionElement.innerHTML = response.data.current.condition.text;
+    humidityElement.innerHTML = `${response.data.current.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.current.wind_kph} Km/h`;
+    temperatureElement.innerHTML = Math.round(temperature);
 }
-
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
-    weatherData(searchInput.value)
-}
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 function formatDate(date) {
     let minutes = date.getMinutes();
     let hours = date.getHours();
     let day = date.getDay();
-  
+
     if (minutes < 10) {
-      minutes = `0${minutes}`;
+        minutes = `0${minutes}`;
     }
-  
+
     if (hours < 10) {
-      hours = `0${hours}`;
+        hours = `0${hours}`;
     }
-  
+
     let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
     ];
-  
+
     let formattedDay = days[day];
     return `${formattedDay} ${hours}:${minutes}`;
-  }
+}
 
-let currentDateELement = document.querySelector("#current-date");
-let currentDate = new Date();
+function searchCity(city) {
+    let apiKey = "29ca9b932a2508dt10ce6d3564ofaf97";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    
+    axios.get(apiUrl)
+        .then(refreshWeather)
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
 
-currentDateELement.innerHTML = formatDate(currentDate);
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-input");
+
+    searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
 
