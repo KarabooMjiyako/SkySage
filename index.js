c726ae9b289c8c892a5ae07734c52b4bb284c076
Karabo Mjiyako <149914891+KarabooMjiyako@users.noreply.github.com>
2023-12-21 21:02:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,62 @@
 function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.current.temp_c;
+    let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#windSpeed");
-    let timeElement = document.querySelector("#current-date");
-    let imageElement = document.querySelector('#icon');
+    let currentDateELement = document.querySelector("#current-date");
+    let currentDate = new Date();
 
-    const weatherImages = {
-        'Clear': 'https://i.ibb.co/0XPnVdc/clear.png',
-        'Rain': 'https://i.ibb.co/FxmRy5n/rain.png',
-        'Snow': 'https://i.ibb.co/zfftwpf/snow.png',
-        'Clouds': 'https://i.ibb.co/R7rNsGd/cloud.png',
-        'Mist': 'https://i.ibb.co/gvGqwG5/mist.png'
-    };
-
-    let weatherMain = response.data.current.condition.text;
-    imageElement.innerHTML = `<img src="${weatherImages[weatherMain] || 'https://i.ibb.co/R7rNsGd/cloud.png'}" class="weather-app-icon" alt="${weatherMain || 'Unknown Weather'}" />`;
-
-    cityElement.innerHTML = response.data.location.name;
-    timeElement.innerHTML = formatDate(new Date(response.data.location.localtime_epoch * 1000));
-    descriptionElement.innerHTML = response.data.current.condition.text;
-    humidityElement.innerHTML = `${response.data.current.humidity}%`;
-    windSpeedElement.innerHTML = `${response.data.current.wind_kph} Km/h`;
-    temperatureElement.innerHTML = Math.round(temperature);
-}
-
-function formatDate(date) {
+    cityElement.innerHTML = response.data.city;
+    currentDateELement.innerHTML = formatDate(currentDate);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    temperatureElement.innerHTML = `${Math.round(temperature)}°C`;
+  }
+  
+  function formatDate(date) {
     let minutes = date.getMinutes();
     let hours = date.getHours();
     let day = date.getDay();
-
+  
     if (minutes < 10) {
-        minutes = `0${minutes}`;
+      minutes = `0${minutes}`;
     }
-
+  
     if (hours < 10) {
-        hours = `0${hours}`;
+      hours = `0${hours}`;
     }
-
+  
     let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
     ];
-
+  
     let formattedDay = days[day];
     return `${formattedDay} ${hours}:${minutes}`;
-}
-
-function searchCity(city) {
+  }
+  
+  function searchCity(city) {
     let apiKey = "29ca9b932a2508dt10ce6d3564ofaf97";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-    
-    axios.get(apiUrl)
-        .then(refreshWeather)
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-}
-
-function handleSearchSubmit(event) {
+    axios.get(apiUrl).then(refreshWeather);
+  }
+  
+  function handleSearchSubmit(event) {
     event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
-
+    let searchInput = document.querySelector("#search-form-input");
+  
     searchCity(searchInput.value);
-}
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("Paris");
-
+  }
+  
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  
+  searchCity("Paris");
