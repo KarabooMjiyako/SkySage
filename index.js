1fa144111d6a4f167c3ea6192f4a87996bdb0d29
Karabo Mjiyako <149914891+KarabooMjiyako@users.noreply.github.com>
2023-12-22 21:51:24 +0200

    Update index.js
    adding forecast data

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ function refreshWeather(response) {
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = `${Math.round(temperature)}°C`;
     imageElement.src = iconUrl;
+    imageElement.alt = weatherMain || 'Unknown Weather';
+
+    getForecast(response.data.city);
   }
   
   function formatDate(date) {
@@ -70,7 +73,53 @@ function refreshWeather(response) {
     searchCity(searchInput.value);
   }
 
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[date.getDay()];
+  }
+  
+  function getForecast(city) {
+    let apiKey = "29ca9b932a2508dt10ce6d3564ofaf97";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+  }
+  
+
+  function displayForecast(response) {
+    let forecastHtml = "";
+  
+    response.data.daily.forEach(function (day, index) {
+      if (index < 5) {
+        forecastHtml =
+          forecastHtml +
+          `
+        <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+  
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>${Math.round(day.temperature.maximum)}º</strong>
+            </div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+            )}º</div>
+          </div>
+        </div>
+      `;
+      }
+    });
+  
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+  }
+  
+
+  
   let searchButtonElement = document.getElementById('bx-search');
   searchButtonElement.addEventListener("click", handleSearchSubmit);
   
-  searchCity("Randfontein");
+  searchCity("Mexico");
+  
