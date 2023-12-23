0a8e0cd807870462c723ff1de12947ece16fe170
Karabo Mjiyako <149914891+KarabooMjiyako@users.noreply.github.com>
2023-12-23 10:49:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -103,66 +103,6 @@ function refreshWeather(response) {
     forecastElement.innerHTML = forecastHtml;
   }
   
-
-  
-  let searchButtonElement = document.getElementById('bx-search');
-  searchButtonElement.addEventListener("click", handleSearchSubmit);
-  
-  searchCity("Randfontein");
-  
-  
-  function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
-  
-    searchCity(searchInput.value);
-  }
-
-  function formatDay(timestamp) {
-    let date = new Date(timestamp * 1000);
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  
-    return days[date.getDay()];
-  }
-  
-  function getForecast(city) {
-    let apiKey = "29ca9b932a2508dt10ce6d3564ofaf97";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-    axios(apiUrl).then(displayForecast);
-  }
-  
-
-  function displayForecast(response) {
-    let forecastHtml = "";
-  
-    response.data.daily.forEach(function (day, index) {
-      if (index < 5) {
-        forecastHtml =
-          forecastHtml +
-          `
-        <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${formatDay(day.time)}</div>
-  
-          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
-          <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-temperature">
-              <strong>${Math.round(day.temperature.maximum)}º</strong>
-            </div>
-            <div class="weather-forecast-temperature">${Math.round(
-              day.temperature.minimum
-            )}º</div>
-          </div>
-        </div>
-      `;
-      }
-    });
-  
-    let forecastElement = document.querySelector("#forecast");
-    forecastElement.innerHTML = forecastHtml;
-  }
-  
-
-  
   let searchButtonElement = document.getElementById('bx-search');
   searchButtonElement.addEventListener("click", handleSearchSubmit);
   
